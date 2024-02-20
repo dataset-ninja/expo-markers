e2362e021a15e6beae0b92450a1670ac631cfcab
peixed351 <peixed351@gmail.com>
2024-02-20 13:56:39 +0300

    add settings

--- src/convert.py
+++ src/convert.py
@@ -1,12 +1,18 @@
-import supervisely as sly
 import os
-from dataset_tools.convert import unpack_if_archive
-import src.settings as s
-from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
 import shutil
+from collections import defaultdict
+from urllib.parse import unquote, urlparse
 
+import numpy as np
+import pycocotools.mask as mask_util
+import supervisely as sly
+from dataset_tools.convert import unpack_if_archive
+from supervisely.io.fs import get_file_name, get_file_name_with_ext
+from supervisely.io.json import load_json_file
 from tqdm import tqdm
+
+import src.settings as s
+
 
 def download_dataset(teamfiles_dir: str) -> str:
     """Use it for large datasets to convert them on the instance"""
@@ -29,7 +35,7 @@ def download_dataset(teamfiles_dir: str) -> str:
             total=fsize,
             unit="B",
             unit_scale=True,
-        ) as pbar:        
+        ) as pbar:
             api.file.download(team_id, teamfiles_path, local_path, progress_cb=pbar)
         dataset_path = unpack_if_archive(local_path)
 
@@ -57,7 +63,8 @@ def download_dataset(teamfiles_dir: str) -> str:
 
         dataset_path = storage_dir
     return dataset_path
-    
+
+
 def count_files(path, extension):
     count = 0
     for root, dirs, files in os.walk(path):
@@ -65,21 +72,134 @@ def count_files(path, extension):
             if file.endswith(extension):
                 count += 1
     return count
-    
+
+
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
     ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
+    real_im_path = "/home/alex/DATASETS/TODO/Expo M arkers/EXPO_HD/real_image_dataset/images"
+    synt_im_path = "/home/alex/DATASETS/TODO/Expo M arkers/EXPO_HD/synt_image_dataset/images"
+    real_json = (
+        "/home/alex/DATASETS/TODO/Expo M arkers/EXPO_HD/real_image_dataset/coco_annotations.json"
+    )
+    synt_json = (
+        "/home/alex/DATASETS/TODO/Expo M arkers/EXPO_HD/synt_image_dataset/coco_annotations.json"
+    )
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
+    batch_size = 30
 
-    # ... some code here ...
+    ds_name_to_data = {"synthetic": (synt_im_path, synt_json), "real": (real_im_path, real_json)}
 
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
+    def convert_rle_mask_to_polygon(rle_mask_data):
+        if type(rle_mask_data["counts"]) is str:
+            rle_mask_data["counts"] = bytes(rle_mask_data["counts"], encoding="utf-8")
+            mask = mask_util.decode(rle_mask_data)
+        else:
+            rle_obj = mask_util.frPyObjects(
+                rle_mask_data,
+                rle_mask_data["size"][0],
+                rle_mask_data["size"][1],
+            )
+            mask = mask_util.decode(rle_obj)
+        mask = np.array(mask, dtype=bool)
+        return sly.Bitmap(mask).to_contours()
 
-    # return project
+    def create_ann(image_path):
+        labels = []
 
+        # image_np = sly.imaging.image.read(image_path)[:, :, 0]
+        # img_height = image_np.shape[0]
+        # img_wight = image_np.shape[1]
 
+        img_height = image_id_to_shape[get_file_name_with_ext(image_path)][0]
+        img_wight = image_id_to_shape[get_file_name_with_ext(image_path)][1]
+
+        if annotations is not None:
+            ann_data = image_name_to_ann[get_file_name_with_ext(image_path)]
+            for curr_data in ann_data:
+                if len(curr_data) != 0:
+                    obj_class = idx_to_class[curr_data[0]]
+
+                    rle_mask_data = curr_data[1]
+
+                    if type(rle_mask_data) is list:
+                        exterior = []
+                        for coords in rle_mask_data:
+                            for i in range(0, len(coords), 2):
+                                exterior.append([coords[i + 1], coords[i]])
+                        poligon = sly.Polygon(exterior)
+                        label_poly = sly.Label(poligon, obj_class)
+                        labels.append(label_poly)
+
+                    else:
+                        polygons = convert_rle_mask_to_polygon(rle_mask_data)
+                        for polygon in polygons:
+                            label = sly.Label(polygon, obj_class)
+                            labels.append(label)
+
+                    bbox_data = list(map(int, curr_data[2]))
+
+                    left = bbox_data[0]
+                    right = bbox_data[0] + bbox_data[2]
+                    top = bbox_data[1]
+                    bottom = bbox_data[1] + bbox_data[3]
+                    rectangle = sly.Rectangle(top=top, left=left, bottom=bottom, right=right)
+                    label = sly.Label(rectangle, obj_class)
+                    labels.append(label)
+
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels)
+
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+
+    red = sly.ObjClass("marker red", sly.AnyGeometry)
+    green = sly.ObjClass("marker green", sly.AnyGeometry)
+    blue = sly.ObjClass("marker blue", sly.AnyGeometry)
+    black = sly.ObjClass("marker black", sly.AnyGeometry)
+
+    idx_to_class = {1: red, 2: green, 3: blue, 4: black}
+
+    meta = sly.ProjectMeta(obj_classes=[red, green, blue, black])
+
+    api.project.update_meta(project.id, meta.to_json())
+
+    for ds_name, ds_data in ds_name_to_data.items():
+        image_id_to_name = {}
+        image_id_to_shape = {}
+        image_name_to_ann = defaultdict(list)
+
+        images_path, ann_json = ds_data
+
+        images_names = os.listdir(images_path)
+
+        json_ann = load_json_file(ann_json)
+
+        images_data = json_ann["images"]
+        for image_data in images_data:
+            image_id_to_name[image_data["id"]] = image_data["file_name"]
+            image_id_to_shape[image_data["file_name"]] = (image_data["height"], image_data["width"])
+
+        annotations = json_ann["annotations"]
+        for ann in annotations:
+            image_name_to_ann[image_id_to_name[ann["image_id"]]].append(
+                [ann["category_id"], ann["segmentation"], ann["bbox"]]
+            )
+
+        dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+
+        progress = sly.Progress("Create dataset {}".format(ds_name), len(images_names))
+
+        for images_names_batch in sly.batched(images_names, batch_size=batch_size):
+            img_pathes_batch = [
+                os.path.join(images_path, im_name) for im_name in images_names_batch
+            ]
+
+            img_infos = api.image.upload_paths(dataset.id, images_names_batch, img_pathes_batch)
+            img_ids = [im_info.id for im_info in img_infos]
+
+            anns = [create_ann(image_path) for image_path in img_pathes_batch]
+            api.annotation.upload_anns(img_ids, anns)
+
+            progress.iters_done_report(len(images_names_batch))
+
+    return project
--- src/settings.py
+++ src/settings.py
@@ -13,37 +13,45 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "Expo Markers"
+PROJECT_NAME_FULL: str = "Expo Markers Dataset"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-APPLICATIONS: List[Union[Industry, Domain, Research]] = None
-CATEGORY: Category = None
+LICENSE: License = License.PubliclyAvailable(
+    source_url="https://github.com/DataGenResearchTeam/expo_markers#License"
+)
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Domain.Surveillance()]
+CATEGORY: Category = Category.Surveillance()
 
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+CV_TASKS: List[CVTask] = [CVTask.InstanceSegmentation(), CVTask.ObjectDetection()]
+ANNOTATION_TYPES: List[AnnotationType] = [
+    AnnotationType.InstanceSegmentation(),
+    AnnotationType.ObjectDetection(),
+]
 
-RELEASE_DATE: Optional[str] = None  # e.g. "YYYY-MM-DD"
+RELEASE_DATE: Optional[str] = "2021-05-23"  # e.g. "YYYY-MM-DD"
 if RELEASE_DATE is None:
     RELEASE_YEAR: int = None
 
-HOMEPAGE_URL: str = None
+HOMEPAGE_URL: str = "https://github.com/DataGenResearchTeam/expo_markers"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 14099604
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/expo-markers"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = {
+    "Part1": "https://expo-markers.s3.amazonaws.com/EXPO_HD/EXPO_HD-20230116T133755Z-001.zip",
+    "Part2": "https://expo-markers.s3.amazonaws.com/EXPO_HD/EXPO_HD-20230116T133755Z-001.zip",
+}
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
@@ -51,16 +59,18 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 
 # If you have more than the one paper, put the most relatable link as the first element of the list
 # Use dict key to specify name for a button
-PAPER: Optional[Union[str, List[str], Dict[str, str]]] = None
-BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = None
-REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = {"GitHub":"some_link_to_repo_if_exists"}
+PAPER: Optional[Union[str, List[str], Dict[str, str]]] = "https://arxiv.org/pdf/2007.14354"
+BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = (
+    "https://datagen.tech/blog/fully-simulated-training-data-how-were-using-synthetic-expo-markers-to-train-a-network/"
+)
+REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = None
 
 CITATION_URL: Optional[str] = None
-AUTHORS: Optional[List[str]] = None
-AUTHORS_CONTACTS: Optional[List[str]] = None
+AUTHORS: Optional[List[str]] = ["Roey Ron", "Gil Elbaz"]
+AUTHORS_CONTACTS: Optional[List[str]] = ["roey.ron@datagen.tech", "gil@datagen.tech"]
 
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = "Datagen Tech, Israel"
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = "https://datagen.tech/"
 
 # Set '__PRETEXT__' or '__POSTTEXT__' as a key with string value to add custom text. e.g. SLYTAGSPLIT = {'__POSTTEXT__':'some text}
 SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = None
