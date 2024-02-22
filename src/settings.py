6948f6466c8f324c2a90d8b55c81899ebd2f6959
peixed351 <peixed351@gmail.com>
2024-02-22 16:35:46 +0300

    release

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME: str = "Expo Markers"
 PROJECT_NAME_FULL: str = "Expo Markers Dataset"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
